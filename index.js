31ee544e9fec62e6bb89e35ed61f8a21d6bf1744
Albinpirela <119135868+Albinpirela@users.noreply.github.com>
2023-03-30 16:59:52 -0400

    Add styles with javascript

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-var modal = document.createElement("div");
-modal.classList.add("modal");
-var image = document.createElement("img");
-var heading = document.createElement("h2")
-var headingTwo = document.createElement("h3")
-var mensaje = document.createElement("p");
-image.src = "img/html5logo.png"
-heading.textContent = "Java Script"
-headingTwo.textContent = "Hola! mundo"
+var modal = document.createElement("div");//crea un div modal
+modal.classList.add("modal");//a ese div le damos la clase modal
+modal.style.display = "flex";//agregamos estilo
+modal.style.alignItems = "center";
+modal.style.justifyContent = "center";
+modal.style.border = "2px solid #aaa";
+modal.style.borderRadius = "10px";
+modal.style.width = "80%";
+modal.style.marginTop = "10em";
+modal.style.margin = "auto"
+var image = document.createElement("img");// con este codigo creamos una variable image que le damos la etiqueta img
+image.style.marginRight = "20px";//add style in img
+image.src = "img/html5logo.png";//add src in img tag
+
+var content = document.createElement("div");//add a var call content and add tag element div
+content.style.display = "flex";//add style
+content.style.flexDirection = "column";
+content.style.justifyContent = "center";
+
+var heading = document.createElement("h2");// to create headind
+heading.textContent = "Java Script";
+
+var headingTwo = document.createElement("h3");
+headingTwo.textContent = "Hola! mundo";
+
+var mensaje = document.createElement("p");// to create mesager
 mensaje.textContent = "que facil es incluir comillas dobles y comillas simples";
-modal.appendChild(heading);
-modal.appendChild(headingTwo);
-modal.appendChild(mensaje);
-modal.appendChild(image);
+
+content.appendChild(heading);//se agrega a content hijo el heading
+content.appendChild(headingTwo);
+content.appendChild(mensaje);
+
+modal.appendChild(image);//se agrega en modal hijo img and the content div
+modal.appendChild(content);
 document.body.appendChild(modal);
